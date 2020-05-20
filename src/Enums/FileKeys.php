7ef3a0a1b6bda183453ab3353e3778d37c2159ce
John Lioneil Dionisio <john.dionisio1@gmail.com>
2020-05-20 23:25:45 +0800

    Add enum keys for File

--- src/Enums/FileKeys.php
+++ src/Enums/FileKeys.php
@@ -6,14 +6,17 @@ abstract class FileKeys
 {
     const ACCESSED = 'accessed';
     const CHANGED = 'changed';
-    const PATHNAME = 'pathname';
-    const TYPE = 'type';
-    const FILESIZE = 'filesize';
-    const SIZE = 'size';
-    const MODIFIED = 'modified';
+    const FILENAME = 'filename';
     const FILEPERMISSIONS = 'filepermissions';
-    const PERMISSION = 'permission';
+    const FILESIZE = 'filesize';
+    const FRAGMENT = 'fragment';
+    const MODIFIED = 'modified';
+    const NAME = 'name';
     const OWNER = 'owner';
+    const PATHNAME = 'pathname';
+    const PERMISSION = 'permission';
+    const SIZE = 'size';
+    const TYPE = 'type';
 
     const DIR_KEY = 'dir';
     const FILE_KEY = 'file';
