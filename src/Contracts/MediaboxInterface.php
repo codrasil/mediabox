4814fb92a6e3df7430d1335bd29302ad2f5837df
John Lioneil Dionisio <john.dionisio1@gmail.com>
2020-05-20 23:27:51 +0800

    Update required methods

--- src/Contracts/MediaboxInterface.php
+++ src/Contracts/MediaboxInterface.php
@@ -7,9 +7,18 @@ interface MediaboxInterface
     /**
      * Retrieve the base path.
      *
+     * @param  string $path
      * @return string
      */
-    public function basePath();
+    public function basePath($path = '');
+
+    /**
+     * Retrieve the root path.
+     *
+     * @param  string $path
+     * @return string
+     */
+    public function rootPath($path = '');
 
     /**
      * Retrieve the list of files and folders.
