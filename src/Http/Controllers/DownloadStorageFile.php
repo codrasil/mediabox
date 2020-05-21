f4b5027dbbf617ffcc0a9fdee928174811b3e178
John Lioneil Dionisio <john.dionisio1@gmail.com>
2020-05-22 00:08:57 +0800

    Add download controller

---
+++ src/Http/Controllers/DownloadStorageFile.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace Codrasil\Mediabox\Http\Controllers;
+
+use Codrasil\Mediabox\Contracts\MediaboxInterface;
+use Codrasil\Mediabox\File;
+use Illuminate\Routing\Controller;
+
+class DownloadStorageFile extends Controller
+{
+    /**
+     * Create a new controller instance.
+     *
+     * @param  \Codrasil\Mediabox\File                        $file
+     * @param  \Codrasil\Mediabox\Contracts\MediaboxInterface $mediabox
+     * @return \Illuminate\Http\Response
+     */
+    public function __invoke(File $file, MediaboxInterface $mediabox)
+    {
+        return $mediabox->download($file);
+    }
+}
